41d03582dd49ede0a07a2f6d3ecd42684dbb89ab
Muhammad Asad <asadghafoor26@gmail.com>
2024-04-09 17:34:00 +0500

    first commit for eeg desktop app. command line input complete

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import logging.handlers
+import json
+
+# def setup():
+#   print("Hello World!")
+#   print("Python version")
+#   print(sys.version)
+
+def main():
+  # setup()
+  print("Hello World!")
